54ad2ba2eb93f68a082b09dba575259dcdc2a85c
rofpitchayuth <rofpitchayuth@gmail.com>
2024-10-01 21:55:46 +0700

    add dropdown when click on profile

--- frontend/components/Nav.tsx
+++ frontend/components/Nav.tsx
@@ -24,7 +24,7 @@ const Nav: React.FC = () => {
                 <a href="/" className="text-black">Home</a>
               </li>
               <li>
-                <a href="/hostel" className="text-black">Hostel</a>
+                <a href="/hotel" className="text-black">Hotel</a>
               </li>
               <li>
                 <a href="/care" className="text-black">Care</a>
@@ -45,7 +45,7 @@ const Nav: React.FC = () => {
           <div className="flex items-center space-x-4">
             <h1 className="text-black">Profile</h1>
             <div className="hidden md:block">
-              <a href="/profile" className="text-black">
+              <button onClick={toggleMenu} className="text-black">
                 <svg
                   className="w-8 h-8"
                   fill="none"
@@ -58,52 +58,62 @@ const Nav: React.FC = () => {
                   <path d="M12 12c2.28 0 4-1.72 4-4s-1.72-4-4-4-4 1.72-4 4 1.72 4 4 4z"></path>
                   <path d="M18 20h-12v-1c0-2 4-3 6-3s6 1 6 3v1z"></path>
                 </svg>
-              </a>
-            </div>
-            
-            {/* Toggle Menu for mobile */}
-            <div className="md:hidden">
-              <button id="menu-toggle" className="text-black" onClick={toggleMenu}>
-                <svg
-                  fill="none"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  viewBox="0 0 24 24"
-                  className="w-6 h-6"
-                >
-                  <path d="M4 6h16M4 12h16M4 18h16"></path>
-                </svg>
               </button>
             </div>
+
+            {isMenuOpen && (
+                  <aside id="default-sidebar" className="fixed top-16 right-0 z-40 w-64 h-screen transition-transform translate-x-0">
+                    <div className="overflow-y-auto py-5 px-3 h-full bg-white-900 border-r border-gray-200">
+                      <ul className="space-y-2">
+                        <li>
+                          <a href="/profile" className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg hover:bg-gray-100">
+                            <span className="ml-3">Profile</span>
+                          </a>
+                        </li>
+                        <li>
+                          <a href="/hotel" className="flex items-center p-2 text-base font-normal text-black-900 rounded-lg hover:bg-gray-100">
+                            <span className="ml-3">Hotel</span>
+                          </a>
+                        </li>
+                        {/* Care button */}
+                        <li>
+                            <button type="button" className="flex items-center p-2 w-full text-base font-normal text-black-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-black dark:hover:bg-gray-100">
+                                  <span className="flex-1 ml-3 text-left">Care</span>
+                                <svg aria-hidden="true" className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                            </button>
+                        </li>
+                        {/* Delivery button */}
+                        <li>
+                            <a href="/delivery" className="flex items-center p-2 w-full text-base font-normal text-black-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-black dark:hover:bg-gray-100">
+                                <span className="flex-1 ml-3 whitespace-nowrap">Delivery</span>
+                            </a>
+                        </li>
+                        {/* Shop button */}
+                        <li>
+                            <button type="button" className="flex items-center p-2 w-full text-base font-normal text-black-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-black dark:hover:bg-grey-700">
+                                <span className="flex-1 ml-3 text-left whitespace-nowrap">Shop</span>
+                                <svg aria-hidden="true" className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                            </button>
+                        </li>
+                        {/* Social button */}
+                        <li>
+                            <a href="/social" className="flex items-center p-2 w-full text-base font-normal text-black-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-black dark:hover:bg-grey-700">
+                                <span className="ml-3">Social</span>
+                            </a>
+                        </li>
+                        <li>
+                            <button type="button" className="flex items-center p-2 w-full text-base font-normal text-black-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-black dark:hover:bg-grey-700" >
+                                <span className="flex-1 ml-3 text-left whitespace-nowrap">Logout</span>
+                            </button>
+                        </li>
+                    </ul>
+                </div>
+              </aside>
+            )}
+            
           </div>
         </div>
       </div>
-
-      {/* Mobile Menu */}
-      {isMenuOpen ? (
-        <ul className="flex-col md:hidden">
-          <li className="px-3 py-3">
-            <a href="/" className="text-black">Home</a>
-          </li>
-          <li className="px-3 py-3">
-            <a href="/hostel" className="text-black">Hostel</a>
-          </li>
-          <li className="px-3 py-3">
-            <a href="/care" className="text-black">Care</a>
-          </li>
-          <li className="px-3 py-3">
-            <a href="/delivery" className="text-black">Delivery</a>
-          </li>
-          <li className="px-3 py-3">
-            <a href="/shop" className="text-black">Shop</a>
-          </li>
-          <li className="px-3 py-3">
-            <a href="/social" className="text-black">Social</a>
-          </li>
-        </ul>
-      ) : null}
     </nav>
 
   );
