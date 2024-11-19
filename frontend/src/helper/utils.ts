9b8a027ec5ff53b9a2c801d32032f32cc908d158
Pitchayuth Jampong <rofpitchayuth@gmail.com>
2024-11-19 15:33:01 +0700

    Add search function in Home page

--- frontend/src/helper/utils.ts
+++ frontend/src/helper/utils.ts
@@ -4,6 +4,8 @@ export function MapArrayToQuery(
   filterAnimal: FilterAnimal[],
   filterSearchCage: FilterSearchCage
 ): string {
+    // console.log("FilterAnimal Input:", JSON.stringify(filterAnimal, null, 2));
+    // console.log("FilterSearchCage Input:", JSON.stringify(filterSearchCage, null, 2));
     let queryParams = "";
     Object.entries(filterSearchCage).forEach(([key, value]) => {
         if (value) {
@@ -18,5 +20,6 @@ export function MapArrayToQuery(
           }
       });
       queryParams = queryParams.slice(-1) === '&' ? queryParams.slice(0, -1) : queryParams;
+      // console.log("Query Params:", queryParams);
       return queryParams;
 }
