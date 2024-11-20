75fe02bb65998c0c0d94e5823fe946db0f5d2ed2
Theetawat Purahong <zopremhero@gmail.com>
2024-11-21 00:54:36 +0700

    edit search

--- frontend/src/components/Hotel-Search/HotelData.tsx
+++ frontend/src/components/Hotel-Search/HotelData.tsx
@@ -2,29 +2,6 @@
 import CardHotel from "./CardHotel";
 import Card from "./Card";
 
-// const hotelList:Hotel[] = [
-//     {
-//         hotel_id: 1,
-//         hotel_name: "A Hotel",
-//         hotel_address: "A Street",
-//         hotel_facility: "Facility:Air, Bed, Open toilet",
-//         img_url: "https://media-cdn.tripadvisor.com/media/photo-s/16/1a/ea/54/hotel-presidente-4s.jpg"
-//     },
-//     {
-//         hotel_id: 2,
-//         hotel_name: "B Hotel",
-//         hotel_address: "B Street",
-//         hotel_facility: "Facility:Air, Bed, Open toilet",
-//         img_url: "https://media-cdn.tripadvisor.com/media/photo-s/16/1a/ea/54/hotel-presidente-4s.jpg"
-//     },
-//     {
-//         hotel_id: 3,
-//         hotel_name: "C Hotel",
-//         hotel_address: "C Street",
-//         hotel_facility: "Facility:Air, Bed, Open toilet",
-//         img_url: "https://media-cdn.tripadvisor.com/media/photo-s/16/1a/ea/54/hotel-presidente-4s.jpg"
-//     },
-// ]
 
 const HotelList = ({hotelList}) => {
     return(
