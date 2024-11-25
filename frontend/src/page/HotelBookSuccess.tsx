463c8f3bfb2ad5e088e3c37cedd965d52e3844b6
Arthit Nopjaroonsri <arthit.nop@gmail.com>
2024-11-25 20:49:26 +0700

    style  payment, select profile

--- frontend/src/page/HotelBookSuccess.tsx
+++ frontend/src/page/HotelBookSuccess.tsx
@@ -10,22 +10,22 @@ export default function HotelBookSuccess() {
       };
 
     return (
-        <div>
+        <div className="h-screen">
             <div className="max-w-2xl w-full mx-auto mt-10">
                 <ol className="flex items-center w-full text-xs text-gray-900 font-medium sm:text-base">
-                    <li className="flex w-full relative text-black after:content-[''] after:w-full after:h-2 after:bg-gray-200 after:inline-block after:absolute lg:after:top-5 after:top-5 after:left-6">
+                    <li className="flex w-full relative text-black after:content-[''] after:w-full after:h-2 after:bg-yellow after:inline-block after:absolute lg:after:top-5 after:top-5 after:left-6">
                         <div className="block whitespace-nowrap z-10 relative">
                             <span className="w-8 h-8 bg-onstep  border-2 border-transparent rounded-full flex justify-center items-center mx-auto mb-3 text-sm text-black lg:w-10 lg:h-10 z-20">1</span>
                             <p className="text-center">Book Detail</p>
                         </div>
                     </li>
-                    <li className="flex w-full relative text-black before:content-[''] before:w-1/2 before:h-2 before:bg-gray-200 before:inline-block before:absolute before:left-0 lg:before:top-5 before:top-5 after:content-[''] after:w-full after:h-2 after:bg-gray-200 after:inline-block after:absolute after:right-0 lg:after:top-5 after:top-5">
+                    <li className="flex w-full relative text-black before:content-[''] before:w-1/2 before:h-2 before:bg-yellow before:inline-block before:absolute before:left-0 lg:before:top-5 before:top-5 after:content-[''] after:w-full after:h-2 after:bg-yellow after:inline-block after:absolute after:right-0 lg:after:top-5 after:top-5">
                         <div className="block whitespace-nowrap z-10 relative">
                             <span className="w-8 h-8 bg-onstep  border-2 border-transparent rounded-full flex justify-center items-center mx-auto mb-3 text-sm text-black lg:w-10 lg:h-10 z-20">2</span>
                             <p className="text-center">Payment</p>
                         </div>
                     </li>
-                    <li className="flex relative text-black before:content-[''] before:w-1/2 before:h-2 before:bg-gray-200 before:inline-block before:absolute before:left-0 lg:before:top-5 before:top-5">
+                    <li className="flex relative text-black before:content-[''] before:w-1/2 before:h-2 before:bg-yellow before:inline-block before:absolute before:left-0 lg:before:top-5 before:top-5">
                         <div className="block whitespace-nowrap z-10 relative">
                             <span className="w-8 h-8 bg-onstep border-2 border-transparent rounded-full flex justify-center items-center mx-auto mb-3 text-sm text-black lg:w-10 lg:h-10 z-20">3</span>
                             <p className="text-center">Book Success</p>
